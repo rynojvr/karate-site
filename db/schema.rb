7892381b8c7f07274f53778aa8acbef34811d1e9
Ryno van Rensburg <rynojvr@yahoo.com>
2018-04-29 14:30:18 +0000

    Add Province/District db_lebel relationships, and ensure that they are all seeded.
    Not yet ued, but gotta start somewhere. :)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180418141417) do
+ActiveRecord::Schema.define(version: 20180429130634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20180418141417) do
     t.string "landline"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "districts", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "province_id"
+    t.index ["province_id"], name: "index_districts_on_province_id"
   end
 
   create_table "members", force: :cascade do |t|
@@ -51,12 +59,6 @@ ActiveRecord::Schema.define(version: 20180418141417) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "regions", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -75,5 +77,6 @@ ActiveRecord::Schema.define(version: 20180418141417) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "districts", "provinces"
   add_foreign_key "members", "clubs"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,155 @@
-# Regions
-puts "[+] Creating Regions..."
-Region.find_or_create_by(name: 'Cape Town')
-Region.find_or_create_by(name: 'The Northern Cape')
-Region.find_or_create_by(name: 'The Western Cape')
-Region.find_or_create_by(name: 'KwaZulu-Natal')
-Region.find_or_create_by(name: 'Free State')
-Region.find_or_create_by(name: 'Gauteng')
-Region.find_or_create_by(name: 'North West Province')
-Region.find_or_create_by(name: 'Mpumalanga')
-Region.find_or_create_by(name: 'Limpopo')
+# Provinces
+puts "[+] Creating Provinces..."
+# Info pulled from https://www.wikiwand.com/en/Districts_of_South_Africa
+provinces = [{
+    name: 'Eastern Cape',
+    districts: [{
+        name: 'Alfred Nzo District Municipality'
+      }, {
+        name: 'Amathole District Municipality'
+      }, {
+        name: 'Buffalo City Metropolitan Municipality'
+      }, {
+        name: 'Chris Hani District Municipality'
+      }, {
+        name: 'Joe Gqabi District Municipality'
+      }, {
+        name: 'Nelson Mandela Bay Metropolitan Municipality'
+      }, {
+        name: 'OR Tambo District Municipality'
+      }, {
+        name: 'Sarah Baartman District Municipality'
+      }
+    ]
+  }, {
+    name: 'Free State',
+    districts: [{
+        name: 'Felize Dabi District Municipality'
+      }, {
+        name: 'Lejweleputswa District Municipality'
+      }, {
+        name: 'Mangaung Metropolitan Municipality'
+      }, {
+        name: 'Thabo Mofutsanyana District Municipality'
+      }, {
+        name: 'XHariep District Municipality'
+      }
+    ]
+  }, {
+    name: 'Gauteng',
+    districts: [{
+        name: 'City of Johannesburg Metropolitan Municipality'
+      }, {
+        name: 'City of Tshwane Metropolitan Municipality'
+      }, {
+        name: 'Ekurhuleni Metropolitan Municipality'
+      }, {
+        name: 'Sedibeng District Municipality'
+      }, {
+        name: 'West Rand District Municipality'
+      }
+    ]
+  }, {
+    name: 'KwaZulu-Natal',
+    districts: [{
+        name: 'Amajuba District Municipality'
+      }, {
+        name: 'eThekwini Metropolitan Municipality'
+      }, {
+        name: 'Harry Gwala District Municipality'
+      }, {
+        name: 'iLembe District Municipality'
+      }, {
+        name: 'King Cetshwayo District Municipality'
+      }, {
+        name: 'Ugu District Municipality'
+      }, {
+        name: 'uMgungundlovu District Municipality'
+      }, {
+        name: 'uMkhanyakude District Municipality'
+      }, {
+        name: 'uMzinyathi District Municipality'
+      }, {
+        name: 'uThukela District Municipality'
+      }, {
+        name: 'Zululand District Municipality'
+      }
+    ]
+  }, {
+    name: 'Limpopo',
+    districts: [{
+        name: 'Capricorn District Municipality'
+      }, {
+        name: 'Mopani District Municipality'
+      }, {
+        name: 'Sekhukune District Municipality'
+      }, {
+        name: 'Vhembe District Municipality'
+      }, {
+        name: 'Waterberg District Municipality'
+      }
+    ]
+  }, {
+    name: 'Mpumalanga',
+    districts: [{
+        name: 'Ehlanzeni District Municipality'
+      }, {
+        name: 'Gert Sibande District Municipality'
+      }, {
+        name: 'Nkangala District Municipality'
+      }
+    ]
+  }, {
+    name: 'North West',
+    districts: [{
+        name: 'Bojanala Platinum District Municipality'
+      }, {
+        name: 'Dr Kenneth Kaunda District Municipality'
+      }, {
+        name: 'Dr Ruth Segomotsi Mompati District Municipality'
+      }, {
+        name: 'Ngaka Modiri Molema District Municipality'
+      }
+    ]
+  }, {
+    name: 'Northern Cape',
+    districts: [{
+        name: 'Frances Baard District Municipality'
+      }, {
+        name: 'John Taolo Gaetsewe District Municipality'
+      }, {
+        name: 'Namakwa District Municipality'
+      }, {
+        name: 'Pixley ka Seme District Municipality'
+      }, {
+        name: 'ZF Mgcawu District Municipality'
+      }
+    ]
+  }, {
+    name: 'Western Cape',
+    districts: [{
+        name: 'Cape Winelands District Municipality'
+      }, {
+        name: 'Central Karoo District Municipality'
+      }, {
+        name: 'City of Cape Town Metropolitan Municipality'
+      }, {
+        name: 'Eden District Municipality'
+      }, {
+        name: 'Overberg District Municipality'
+      }, {
+        name: 'West Coast District Municipality'
+      }
+    ]
+  }
+]
+
+provinces.each do |prov_data|
+  prov = Province.find_or_create_by(name: prov_data[:name])
+  prov_data[:districts].each do |district_data|
+    prov.districts.find_or_create_by(district_data)
+  end
+end
 
 puts "[+] Creating clubs..." if Club.count < 7
 while Club.count < 7 do
